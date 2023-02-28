4f7e69043a680cd1573df3e958ec6a737e79240b
Ivan Silva <silvaivan280@gmail.com>
2023-02-28 14:42:16 -0300

    solucion del ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+let numeroUno = parseInt(prompt("Ingrese un número:"));
+let numeroDos = parseInt(prompt("Ingrese otro número:"));
+document.write('El resultado de la suma es: ',numeroUno + numeroDos);
