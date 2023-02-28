9e7b9000c19f11f8fdbdbcc1783efe26c1b0a7b8
Ivan Silva <silvaivan280@gmail.com>
2023-02-28 14:50:56 -0300

    solucion del ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+let numeroUno = parseInt(prompt("Ingrese un número:"));
+let numeroDos = parseInt(prompt("Ingrese otro número:"));
+
+if (numeroUno > numeroDos){
+    document.write(`El número ${numeroUno} es mayor`);
+}else{
+    document.write(`El número ${numeroDos} es mayor`);
+}
